d28b8fe974ba6c2722c086363f547483eb9ceab8
Marek Czyż <Marek.Czyz@dreamlab.pl>
2019-10-08 08:49:25 +0200

    Getting started

--- keystone.js
+++ keystone.js
@@ -2,12 +2,15 @@ const keystone = require('keystone');
 
 keystone.init({
     'cookie secret': 'secure string goes here',
+    'views': 'templates/views',
+    'view engine': 'pug',
     'name': 'blog',
     'user model': 'User',
+    'event model': 'Event',
     'auto update': true,
     'auth': true,
   });
 
+keystone.set('routes', require('./routes'));
 keystone.import('models');
-
 keystone.start();
